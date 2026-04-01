9174729cb4bbb170c60f344015c9b9c7f4be6a3f
bhattaraiprazwall <111439854+bhattaraiprazwall@users.noreply.github.com>
2026-04-01 12:54:43 +0545

    Create function made dynamic

--- lib/functions/db_functions.dart
+++ lib/functions/db_functions.dart
@@ -1,10 +1,38 @@
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:logger/logger.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+var logger = Logger();
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+// static
+// create() async {
+//   try {
+//     await FirebaseFirestore.instance
+//         .collection('fruits')
+//         .doc('pineapple')
+//         .set({
+//       'brand': 'malbok',
+//       'year': 2025,
+//       'price': 150
+//     });
+
+//     if (kDebugMode) {
+//       logger.d('Record Created');
+//     }
+//   } catch (e) {
+//     logger.e('Error: $e');
+//   }
+// }
+
+
+// dynamic
+create (String collection,doc,name,animal,int age) async{
+  try{
+    await FirebaseFirestore.instance.collection(collection).doc(doc).set({'name':name,'animal':animal,'age':age});
+          logger.d('Record Created');
+
+  }
+  catch(e)
+  {
+    logger.e('Error:$e');
   }
 }

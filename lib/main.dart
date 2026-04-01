11a9961c3d6458b3a491c7a1dd5523db3ac3b0a9
bhattaraiprazwall <111439854+bhattaraiprazwall@users.noreply.github.com>
2026-04-01 12:55:17 +0545

    Controlled debug message level

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_widgets/firebase/firebase_email_pass_authentication.dart';
 import 'package:flutter_widgets/firestore_db/database_options.dart';
 // import 'package:flutter_widgets/pages/insta_profile_page.dart';
@@ -23,35 +24,37 @@ void main() async {
   await Firebase.initializeApp();
   // Set Firebase language code to eliminate "X-Firebase-Locale is null" warning
   FirebaseAuth.instance.setLanguageCode('en');
+  // Disable verbose Firebase logging
+
+  FirebaseAuth.instance.setLanguageCode('en');
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-    home:  StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(), builder: 
-    (context, snapshot) {
-      if(snapshot.hasData)
-      {
-        return DatabaseOptions(); 
-      }
-      else{
-        return FirebaseEmailPassAuthentication();
-      }
-    },
-    
-    ),
+    return MaterialApp(
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return DatabaseOptions();
+          } else {
+            return FirebaseEmailPassAuthentication();
+          }
+        },
+      ),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        brightness: Brightness.light,primaryColor: Colors.blue,
+        brightness: Brightness.light,
+        primaryColor: Colors.blue,
         // scaffoldBackgroundColor:Color.fromRGBO(240, 242, 245, 1),
         textTheme: TextTheme(
-          titleMedium: TextStyle(fontWeight: FontWeight.bold)
-        )
+          titleMedium: TextStyle(fontWeight: FontWeight.bold),
+        ),
       ),
-      
     );
   }
 }
